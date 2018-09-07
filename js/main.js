f90e20e60f34717b4a94abf3d0b41567e55744b8
Missionary <missionarymc@gmail.com>
2018-09-06 21:50:06 -0700

    Updates

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // ===== Scroll to Top ====
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
         $('#back-to-the-top').fadeIn(200);    // Fade in the arrow
     } else {
         $('#back-to-the-top').fadeOut(200);   // Else fade out the arrow
     }
 });
-$('#back-to-the-top').click(function() {      // When arrow is clicked
+$('#back-to-the-top').click(function () {      // When arrow is clicked
     $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
+        scrollTop: 0                       // Scroll to top of body
     }, 500);
 });
